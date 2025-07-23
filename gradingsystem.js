5a3d508b3619d8223cfe85bc3e5fb77c09996663
Collins299r <collinstobennaeze@gmail.com>
2025-07-23 01:40:01 +0100

    Update gradingsystem.js

--- gradingsystem.js
+++ gradingsystem.js
@@ -1,7 +1,9 @@
-function getGrade(score)
+function getGrade(score) {
   if (score >= 70 && score <= 100) return 'A';
-  if (score >= 60 && score <= 69) return 'B';
-  if (score >= 50 && score <= 59) return 'C';
-  if (score >= 45 && score <= 49) return 'D';
-  if (score >= 40 && score <= 44) return 'E';
-  if (score >= 0 && score <= 39) return 'F'. 
+  else if (score >= 60 && score <= 69) return 'B';
+  else if (score >= 50 && score <= 59) return 'C';
+  else if (score >= 45 && score <= 49) return 'D';
+  else if (score >= 40 && score <= 44) return 'E';
+  else if (score >= 0 && score <= 39) return 'F';
+  else return 'Invalid score';
+}
